4edce98680538ece63fcfc009c78f249d7777edc
Aruntgym <arunkumarg7845@gmail.com>
2024-10-17 10:47:46 +0530

    "Third commit"

--- vite-project/src/components/Sidebar.jsx
+++ vite-project/src/components/Sidebar.jsx
@@ -4,9 +4,10 @@ import "./Sidebar.css"; // Create a CSS file for sidebar styling
 const Sidebar = ({ onOptionSelect }) => {
   return (
     <div className="sidebar">
+      <h1>Create</h1>
       <div className="sidebar-item" onClick={() => onOptionSelect("table")}>
         <div className="icon">📝</div>
-        <span>Create</span>
+        <span>Table</span>
       </div>
       <div className="sidebar-item" onClick={() => onOptionSelect("form")}>
         <div className="icon">📋</div>
